efab182eb311634c0115f564990ac5286bdace7f
Danny Smith <hi@danny.is>
2017-08-28 20:06:33 +0100

    added user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    first_name { Faker::Name.name }
+    surname { Faker::Name.last_name }
+    sequence(:email) { |n| "person#{n}@example.com" }
+    password { Faker::Internet.password(8) }
+  end
+end
